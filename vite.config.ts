44267b51798afa14ee87e513c8e7b005f260c04a
miguelangelfeo <mfeomerchan@gmail.com>
2026-05-25 16:11:02 -0500

    Devuelve todo

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+
+export default defineConfig()
